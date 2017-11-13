41ec1ba986308744ff9d747e07f3c70314bd8e81
dreamiland <maple_jista@hotmail.com>
2017-11-13 00:49:09 -0600

    Add project files.

---
+++ Actividad12/Repetitions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad12
+{
+    [Serializable]
+    class Repetitions
+    {
+        private string word;
+        private int repetitions;
+        private HashSet<string> repeatsPerFile = new HashSet<string>();
+
+        public Repetitions(string w, string fileName)
+        {
+            word = w;
+            repetitions = 1;
+            repeatsPerFile.Add(fileName);
+        }
+
+        public void addWord(string fileName)
+        {
+            repetitions++;
+            repeatsPerFile.Add(fileName);
+        }
+
+        public override bool Equals(object rc)
+        {
+            return Equals(rc as Repetitions);
+        }
+
+        public int getRepetitions()
+        {
+            return this.repetitions;
+        }
+
+        public int getRepeatsCount()
+        {
+            return this.repeatsPerFile.Count();
+        }
+
+        public HashSet<string> getFilesFoundIn()
+        {
+            return this.repeatsPerFile;
+        }
+    }
+}
